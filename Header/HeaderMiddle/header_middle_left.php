a1519e2bf0a45d0dbbcfa525afdd16d57ab7db77
Zuyhaidao <haimly123@gmail.com>
2025-02-10 13:39:51 +0700

    BTL WEB

---
+++ Header/HeaderMiddle/header_middle_left.php
@@ -0,0 +1,7 @@
+<div class="header-middle-left">
+                    <div class="header-logo">
+                        <a href="">
+                            <img src="./assets/img/xuafood.png" alt="" class="header-logo-img">
+                        </a>
+                    </div>
+</div>
